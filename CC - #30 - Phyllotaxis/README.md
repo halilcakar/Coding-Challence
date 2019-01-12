f42d59d337a0e724e415cc90cec093d4b72cd19c
Halil ÇAKAR <hcakar.1992@gmail.com>
2019-01-12 17:41:14 +0300

    Created Phyllotaxis Example

---
+++ CC - #30 - Phyllotaxis/README.md
@@ -0,0 +1,12 @@
+## #30 - Phyllotaxis
+All codes are belongs to [@shiffman](https://github.com/shiffman)
+
+Coding Train: [Phyllotaxis](https://github.com/CodingTrain/website/tree/master/CodingChallenges/CC_030_Phyllotaxis)
+
+Youtube Channel: [The Coding Train - Phyllotaxis](https://www.youtube.com/watch?v=KWoJgHFYWxY&index=33&list=PLRqwX-V7Uu6ZiZxtDDRCi6uhfTH4FilpH)
+
+thecodingtrain.com: [Website](https://thecodingtrain.com/CodingChallenges/030-phyllotaxis.html)
+
+I'm simply writing all code again, using [phaser3](https://phaser.io/).
+
+![]()
---
+++ CC - #30 - Phyllotaxis/game.js
@@ -0,0 +1,57 @@
+/**
+ * comment
+ * */
+var game;
+window.onload = function() {
+	const gameConfig = {
+		width: 400,
+		height: 400,
+		resolution: 1,
+		scene: [ SimpleScene ]
+	};
+	game = new Phaser.Game(gameConfig);
+	window.focus();
+	resizeGame();
+	window.addEventListener('resize', resizeGame);
+};
+
+class SimpleScene extends Phaser.Scene {
+	constructor() {
+		super({key: 'SimpleScene'});
+	}
+	create() {
+		this.n = 0;
+		this.c = 3;
+		this.angle = 137.6;
+		//137.3
+		//137.6
+		this.grap = this.add.graphics();
+
+	}
+	update() {
+		if(this.n < this.game.config.width * 2) {
+			let a = this.n * this.angle;
+			let r = this.c * Math.sqrt(this.n);
+			let x = r * Math.cos(a) + this.game.config.width / 2;
+			let y = r * Math.sin(a) + this.game.config.height / 2;
+			this.grap.fillStyle(new Phaser.Display.Color.RandomRGB(100, 255).color, 1);
+			this.grap.fillCircle(x, y, 2, 2);
+			this.n++;
+		}
+	}
+}
+
+const resizeGame = () => {
+	var canvas = document.querySelector('canvas');
+	var windowWidth = window.innerWidth;
+	var windowHeight = window.innerHeight;
+	var windowRatio = windowWidth / windowHeight;
+	var gameRatio = game.config.width / game.config.height;
+	if (windowRatio < gameRatio) {
+		canvas.style.width = windowWidth + 'px';
+		canvas.style.height = (windowWidth / gameRatio) + 'px';
+	} else {
+		canvas.style.width = (windowHeight * gameRatio) + 'px';
+		canvas.style.height = windowHeight + 'px';
+	}
+};
